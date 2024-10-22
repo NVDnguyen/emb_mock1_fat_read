6cd1084b061ef930a04e5f4493ee4e48fb1783f4
NVDnguyen <donvhe172638@fpt.edu.vn>
2024-10-22 20:56:50 +0700

    update

--- src/status_code.h
+++ src/status_code.h
@@ -1,22 +1,20 @@
 #ifndef STATUS_CODES_H
 #define STATUS_CODES_H
-
-#define PATH_FILE "data/floppy.img"
-
+#define FILE_PATH "data/floppy.img"
 typedef enum
 {
     ERROR,
     OK,
-    FILE_NULL_ERROR,
-    READ_ERROR,
-    MEMORY_ALLOCATION_ERROR
+    ERROR_NULL_FILE,
+    ERROR_READ,
+    ERROR_MEMORY_ALLOCATION
 } status_t;
 
 static const char* status_strings[] = {
     "ERROR",
     "OK",
-    "FILE_NULL_ERROR",
-    "READ_ERROR",
-    "MEMORY_ALLOCATION_ERROR"
+    "ERROR_NULL_FILE",
+    "ERROR_READ",
+    "ERROR_MEMORY_ALLOCATION"
 };
 #endif /*STATUS_CODES_H*/ 
